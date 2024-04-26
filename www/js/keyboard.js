feb39edb0980e8c592c081f4def084ad9d3bc8bd
kpodjuk <62662345+kpodjuk@users.noreply.github.com>
2024-04-26 14:35:20 +0200

    handle ws reconnect

--- www/js/keyboard.js
+++ www/js/keyboard.js
@@ -1,36 +1,26 @@
 let wsConnectionAddress = "ws://" + "key.local" + ":81/";
 let wsConnectionName = ["arduino"];
 
-var connectionCheckInterval = window.setInterval(function () {
-  checkConnection();
-}, 1000);
-
-var connection = new WebSocket(wsConnectionAddress, wsConnectionName);
+// var connectionCheckInterval = window.setInterval(function () {
+//   checkConnection();
+// }, 1000);
+var connection;
+var reconnectInterval;
+connect();
+function connect() {
+  connection = new WebSocket(wsConnectionAddress, wsConnectionName, 2000);
+  updateStatusDot();
+}
 
 // add event listeners to all keys
-document.querySelectorAll(".key").forEach((key) => {
-  key.addEventListener("click", () => {
-    sendKeyPress(key.innerHTML);
+document
+  .getElementById("keyboard")
+  .querySelectorAll("button")
+  .forEach((key) => {
+    key.addEventListener("click", () => {
+      sendKeyPress(key.innerHTML);
+    });
   });
-});
-
-connection.onopen = function () {
-  // let answerJson = {
-  //   connected: true,
-  // };
-  // sendJSON(answerJson);
-};
-connection.onerror = function (error) {
-  console.log("WebSocket Error ", error);
-};
-connection.onmessage = function (e) {
-  console.log("Message received:" + e.data);
-  processWebsocketMessage(e.data);
-};
-connection.onclose = function () {
-  checkConnection();
-  console.log("WebSocket connection closed");
-};
 
 function sendJSON(message) {
   // input : message object
@@ -45,7 +35,7 @@ function processWebsocketMessage(message) {
   console.log(message);
 }
 
-function checkConnection() {
+function updateStatusDot() {
   // 0	CONNECTING	Socket has been created. The connection is not yet open.
   // 1	OPEN	The connection is open and ready to communicate.
   // 2	CLOSING	The connection is in the process of closing.
@@ -65,16 +55,19 @@ function checkConnection() {
       break;
     case 3:
       document.getElementById("statusDot").style.backgroundColor = "red";
-      setTimeout(function () {
-        // Try to reconnect
-        connection = new WebSocket(wsConnectionAddress, wsConnectionName);
-      }, 5000);
+
+      // probably will keep on trying to create new connections, kinda bad
+      // setTimeout(function () {
+      //   // Try to reconnect
+      //   connection = new WebSocket(wsConnectionAddress, wsConnectionName);
+      // }, 5000);
       break;
   }
 }
 
 function sendKeyPress(key) {
   let answerJson = {
+    type: "keyPress",
     keyPressed: key,
   };
   sendJSON(answerJson);
